633ee7659f4208b47190e39f4142a91066d68f0a
Shaxa8000 <shokhbozyakhyaev95@gmail.com>
2023-09-14 14:21:02 +0500

    navbar done

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import { Container } from './style';
+import {Container, Wrapper, Section, Logo, Link, Main} from './style';
+import { navbar } from '../../utils/navbar';
+import {Outlet, useNavigate} from 'react-router-dom';
+
 
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
-    <Container>Navbar</Container>
+    <Container>
+      <Main>
+        <Wrapper>
+          <Section onClick={()=> navigate('/home')} logo={true}>
+             <Logo/><h3>Houzing</h3>
+          </Section>
+          <Section>
+            {navbar.map(({title, path, id}, index) => {
+              return <Link className={({isActive}) => isActive && 'active'} key={index} to={path}>{title}</Link>
+            })}
+          </Section>
+          <Section>
+            <button>Sign In</button>
+          </Section>
+        </Wrapper>
+      </Main>
+        <Outlet/>
+    </Container>
   )
-}
+};
 
 export default Navbar
--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { navbar } from '../utils/navbar';
+import Navbar from '../components/Navbar';
 
 const Root = () => {
   return (
-    <div>Root</div>
+    <BrowserRouter>
+       <Routes>
+        <Route element={<Navbar/>} >
+          {
+            navbar.map(({title, path, element, id}) => {
+                return <Route key={id} path={path} element={element}/>
+            })
+          }
+        </Route>
+          <Route path='*' element={<h1>404 NOT FOUND</h1>}/>
+          <Route path='/' element={<Navigate to='/home'/>} />
+       </Routes>
+    </BrowserRouter>
   )
 }
 
